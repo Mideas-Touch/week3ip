4a8e026836d3b82b8e6b04572810c4cf72cacc43
fliblip <wickynjy@gmail.com>
2022-07-17 17:06:10 +0300

    added monthly data table

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
 // alert("You have 1 pending tasks")
 
-    var shedA = 300;
-    var shedB = 250;
-    var shedC = 400;
-    var shedD = 305;
+//     var shedA = 300;
+//     var shedB = 250;
+//     var shedC = 400;
+//     var shedD = 305;
 
-dailyProduction = (shedA+shedB+shedC+shedD);
-weeklyProduction = (dailyProduction * 7);
-monthlyProduction = (weeklyProduction * 4);
+// dailyProduction = (shedA+shedB+shedC+shedD);
+// weeklyProduction = (dailyProduction * 7);
+// monthlyProduction = (weeklyProduction * 4);
 
 
-function incomeOverAmonth(buyingPrice) {
-    if(buyingPrice == 1) {
-        return buyingPrice;
-    }
-    else{
-        return buyingPrice * monthlyProduction(buyingPrice)
-    }
-console.log(incomeOverAmonth)
-}
+// function incomeOverAmonth(buyingPrice) {
+//     if(buyingPrice == 1) {
+//         return buyingPrice;
+//     }
+//     else{
+//         return buyingPrice * monthlyProduction(buyingPrice)
+//     }
+// console.log(incomeOverAmonth)
+// }
 
-const body = document.body
+// const body = document.body
 
 
 
