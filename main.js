69e068c262a34a3af40eb072c8c124b529278036
fliblip <wickynjy@gmail.com>
2022-07-17 20:40:22 +0300

    created leap year checker

--- main.js
+++ main.js
@@ -25,6 +25,20 @@
 // const shedA = document.append("250")
 // console.log(shedA)
 
+function dailyProduction() {
+    var first = document.getElementById("shed1");
+    var second = document.getElementById("shed2");
+    var third = document.getElementById("shed3");
+    var fourth = document.getElementById("shed4");
+
+    var i;
+    for (i = 0; i < first.length ;i++) {
+        text += first.elements[i].value;
+    }
+
+    console.log(first)
+}
+
 
 
 function checkLeap(year) {
